b7b6dc7d8cc20bee5679680f6f9d1ec8f34b80c7
Yeung Yiu Hung <hkclex@gmail.com>
2016-02-28 00:33:55 +0800

    Inital Commit

---
+++ Example/Pods/Target Support Files/Pods-PokemonKit_Example/Pods-PokemonKit_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PokemonKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PokemonKit_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/PokemonKit/PokemonKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PokemonKit : NSObject
+@end
+@implementation PodsDummy_PokemonKit
+@end
