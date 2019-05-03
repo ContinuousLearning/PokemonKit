21b4b9be3a149df4ad61faf63df9570e6ad5cc0b
Nityananda Zbil <nityananda.zbil@icloud.com>
2019-05-03 17:10:12 +0200

    Testing
    from branch testing
    
    commit 8842edc3d54f71295ea860cadc08bafce3b6439e
    Author: Nityananda Zbil <nityananda.zbil@icloud.com>
    Date:   Fri May 3 17:09:05 2019 +0200
    
        correct type mismatches
    
    commit b4a5ff28c5053f1f7290bb65948abe66bf5b80ec
    Author: Nityananda Zbil <nityananda.zbil@icloud.com>
    Date:   Fri May 3 16:32:24 2019 +0200
    
        correct two type mismatches
    
    commit 4abcee96c69934a70707c762109c99e469308de8
    Author: Nityananda Zbil <nityananda.zbil@icloud.com>
    Date:   Fri May 3 16:25:24 2019 +0200
    
        find failing calls
    
    commit 15f4f76af8a2f0ad77a94cd9a59d7630d69c9342
    Author: Nityananda Zbil <nityananda.zbil@icloud.com>
    Date:   Fri May 3 16:06:07 2019 +0200
    
        add tests

--- Classes/PokemonKit.swift
+++ Classes/PokemonKit.swift
@@ -10,11 +10,11 @@ import Foundation
 import PromiseKit
 import Moya
 
-// MARK: Constant
+// - MARK: - Constant
 
 let baseURL: String = "http://pokeapi.co/api/v2"
 
-// MARK: Classes
+// MARK: - Classes
 
 /// Languages for translations of API resource information.
 open class PKMLanguage: Codable {
@@ -505,7 +505,7 @@ open class PKMPokemonType: Codable {
     open var slot: Int?
     
     /// The type the referenced Pokémon has
-    open var type: String?
+    open var type: PKMNamedAPIResource?
     
 }
 
@@ -813,7 +813,7 @@ open class PKMAbility: Codable {
 open class PKMRegion: Codable {
     
     /// The identifier for this region resource
-    open var id: Bool?
+    open var id: Int?
     
     /// The name for this region resource
     open var name: String?
@@ -885,7 +885,7 @@ open class PKMLocation: Codable {
     open var gameIndices: [PKMGenerationGameIndex]?
     
     /// Areas that can be found within this location
-    open var areas: PKMAPIResource?
+    open var areas: [PKMNamedAPIResource]?
     
 }
 
@@ -1364,7 +1364,7 @@ open class PKMVerboseEffect: Codable {
     open var shortEffect: String?
     
     /// The language this effect is in
-    open var language: Bool?
+    open var language: PKMNamedAPIResource?
     
 }
 
@@ -1468,7 +1468,7 @@ open class PKMVersionGroup: Codable {
     open var order: Int?
     
     /// The generation this version was introduced in
-    open var generation: [PKMNamedAPIResource]?
+    open var generation: PKMNamedAPIResource?
     
     /// A list of methods in which Pokémon can learn moves in this version group
     open var moveLearnMethods: [PKMNamedAPIResource]?
@@ -1705,7 +1705,7 @@ open class PKMEncounterConditionValue: Codable {
     open var name: String?
     
     /// The condition this encounter condition value pertains to
-    open var condition: [PKMNamedAPIResource]?
+    open var condition: PKMNamedAPIResource?
     
     /// The name of this encounter condition value listed in different languages
     open var names: [PKMName]?
@@ -1753,7 +1753,7 @@ open class PKMSuperContestEffect: Codable {
     open var id: Int?
     
     /// The level of appeal this super contest effect has
-    open var appeal: String?
+    open var appeal: Int?
     
     /// The flavor text of this super contest effect listed in different languages
     open var flavorTextEntries: [PKMFlavorText]?
@@ -1794,7 +1794,7 @@ open class PKMContestEffect: Codable {
     open var id: Int?
     
     /// The base number of hearts the user of this move gets
-    open var appeal: String?
+    open var appeal: Int?
     
     /// The base number of hearts the user's opponent loses
     open var jam: Int?
@@ -1963,7 +1963,7 @@ open class PKMBerryFirmness: Codable {
 
 }
 
-// MARK: PokeAPI
+// MARK: - PokeAPI
 public enum PokeAPI: TargetType {
     case berryList
     case berry(id: String)
@@ -2367,8 +2367,9 @@ extension PokeAPI {
         return nil
     }
 }
-// MARK: -
-// MARK: Functions
+
+// MARK: - Functions
+// MARK: Internal
 let provider = MoyaProvider<PokeAPI>()
 let decoder = JSONDecoder()
 
@@ -2392,6 +2393,13 @@ internal func decode<T: Decodable>(_ promise: Promise<Response>) -> Promise<T> {
     })
 }
 
+internal func hide<T>(_ promise: Promise<T>) -> Promise<Void> {
+    return promise.map({ _ in
+        return ()
+    })
+}
+
+// MARK: Public
 /**
  Fetch Berry list
 
@@ -3330,4 +3338,132 @@ public func fetchLanguages() -> Promise<PKMPagedObject> {
  */
 public func fetchLanguage(_ languageId: String) -> Promise<PKMLanguage>{
     return decode(request(target: .language(id: languageId)))
+}
+
+// MARK: - Tests
+public func testAll() {
+    testAllLists()
+    testAllParameter()
+}
+
+public func testAllParameter() {
+    print("testAllParameter")
+    let promises: [Promise<Void>] = [
+        hide(fetchBerry("1")),
+        hide(fetchBerryFirmness("1")),
+        hide(fetchBerryFlavour("1")),
+        hide(fetchContestType("1")),
+        hide(fetchContestEffect("1")),
+        hide(fetchSuperContestEffect("1")),
+        hide(fetchEncounterMethod("1")),
+        hide(fetchEncounterCondition("1")),
+        hide(fetchEncounterConditionValue("1")),
+        hide(fetchEvolutionChain("1")),
+        hide(fetchEvolutionTrigger("1")),
+        hide(fetchGeneration("1")),
+        hide(fetchPokedex("1")),
+        hide(fetchVersion("1")),
+        hide(fetchVersionGroup("1")),
+        hide(fetchItem("1")),
+        hide(fetchItemAttribute("1")),
+        hide(fetchItemCategory("1")),
+        hide(fetchItemFlingEffect("1")),
+        hide(fetchItemPocket("1")),
+        hide(fetchMove("1")),
+        hide(fetchMoveAilment("1")),
+        hide(fetchMoveBattleStyle("1")),
+        hide(fetchMoveCategory("1")),
+        hide(fetchMoveDamageClass("1")),
+        hide(fetchMoveLearnMethod("1")),
+        hide(fetchMoveTarget("1")),
+        hide(fetchLocation("1")),
+        hide(fetchLocationArea("1")),
+        hide(fetchPalParkArea("1")),
+        hide(fetchRegion("1")),
+        hide(fetchAbility("1")),
+        hide(fetchCharacteristic("1")),
+        hide(fetchEggGroup("1")),
+        hide(fetchGender("1")),
+        hide(fetchGrowthRate("1")),
+        hide(fetchNature("1")),
+        hide(fetchPokeathlonStat("1")),
+        hide(fetchPokemon("1")),
+        hide(fetchPokemonColor("1")),
+        hide(fetchPokemonForm("1")),
+        hide(fetchPokemonHabitat("1")),
+        hide(fetchPokemonShape("1")),
+        hide(fetchPokemonSpecies("1")),
+        hide(fetchStat("1")),
+        hide(fetchType("1")),
+        hide(fetchLanguage("1")),
+        ]
+
+    promises.enumerated().forEach { (offset, promise) in
+        promise.done({ (_) in
+            print("\(offset): all good")
+        }).catch({ (error) in
+            print("\(offset): \(error)")
+        })
+    }
+}
+
+public func testAllLists() {
+    print("testAllLists")
+    let promises: [Promise<Void>] = [
+        hide(fetchBerryList()),
+        hide(fetchBerryFirmnessList()),
+        hide(fetchBerryFlavours()),
+        hide(fetchContestList()),
+        hide(fetchContestEffects()),
+        hide(fetchSuperContestEffects()),
+        hide(fetchEncounterMethods()),
+        hide(fetchEncounterConditions()),
+        hide(fetchEncounterConditionValues()),
+        hide(fetchEvolutionChains()),
+        hide(fetchEvolutionTriggers()),
+        hide(fetchGenerations()),
+        hide(fetchPokedexes()),
+        hide(fetchVersions()),
+        hide(fetchVersionGroups()),
+        hide(fetchItems()),
+        hide(fetchItemAttributes()),
+        hide(fetchItemCategories()),
+        hide(fetchItemFlingEffects()),
+        hide(fetchItemPockets()),
+        hide(fetchMoves()),
+        hide(fetchMoveAilments()),
+        hide(fetchMoveBattleStyles()),
+        hide(fetchMoveCategories()),
+        hide(fetchMoveDamageClasses()),
+        hide(fetchMoveLearnMethods()),
+        hide(fetchMoveTargets()),
+        hide(fetchLocations()),
+        hide(fetchLocationAreas()),
+        hide(fetchPalParkAreas()),
+        hide(fetchRegions()),
+        hide(fetchAbilities()),
+        hide(fetchCharacteristics()),
+        hide(fetchEggGroup()),
+        hide(fetchGenders()),
+        hide(fetchGrowthRates()),
+        hide(fetchNatures()),
+        hide(fetchPokeathlonStats()),
+        hide(fetchPokemons()),
+        hide(fetchPokemonColors()),
+        hide(fetchPokemonForms()),
+        hide(fetchPokemonHabitats()),
+        hide(fetchPokemonShapes()),
+        hide(fetchPokemonSpecies()),
+        hide(fetchStats()),
+        hide(fetchType()),
+        hide(fetchLanguages()),
+        ]
+
+    promises.enumerated().forEach { (offset, promise) in
+        promise.done({ (_) in
+            print("\(offset): all good")
+        }).catch({ (error) in
+            print("\(offset): \(error)")
+        })
+    }
 }
