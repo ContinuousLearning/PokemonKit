54973b5c43b56a239559ae27feb1253f4e899937
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-05-22 15:44:32 +0200

    Update _PokemonKitTests.swift

--- Tests/PokemonKitTests/_PokemonKitTests.swift
+++ Tests/PokemonKitTests/_PokemonKitTests.swift
@@ -10,6 +10,58 @@ import XCTest
 @testable import PokemonKit
 
 class _PokemonKitTests: XCTestCase {
+
+    typealias Signature = (@escaping (Result<Int, Error>) -> Void) -> ()
+    
+    let list: [Signature] = [
+        PKMAbility.fetchCount,
+        PKMBerry.fetchCount,
+        PKMBerryFirmness.fetchCount,
+        PKMBerryFlavour.fetchCount,
+        PKMCharacteristic.fetchCount,
+        PKMContestEffect.fetchCount,
+        PKMContestType.fetchCount,
+        PKMEggGroup.fetchCount,
+        PKMEncounterCondition.fetchCount,
+        PKMEncounterConditionValue.fetchCount,
+        PKMEncounterMethod.fetchCount,
+        PKMEvolutionChain.fetchCount,
+        PKMEvolutionTrigger.fetchCount,
+        PKMGender.fetchCount,
+        PKMGeneration.fetchCount,
+        PKMGrowthRate.fetchCount,
+        PKMItem.fetchCount,
+        PKMItemAttribute.fetchCount,
+        PKMItemCategory.fetchCount,
+        PKMItemFlingEffect.fetchCount,
+        PKMItemPocket.fetchCount,
+        PKMLanguage.fetchCount,
+        PKMLocation.fetchCount,
+        PKMLocationArea.fetchCount,
+        PKMMove.fetchCount,
+        PKMMoveAilment.fetchCount,
+        PKMMoveBattleStyle.fetchCount,
+        PKMMoveCategory.fetchCount,
+        PKMMoveDamageClass.fetchCount,
+        PKMMoveLearnMethod.fetchCount,
+        PKMMoveTarget.fetchCount,
+        PKMNature.fetchCount,
+        PKMPalParkArea.fetchCount,
+        PKMPokeathlonStat.fetchCount,
+        PKMPokedex.fetchCount,
+        PKMPokemon.fetchCount,
+        PKMPokemonColor.fetchCount,
+        PKMPokemonForm.fetchCount,
+        PKMPokemonHabitat.fetchCount,
+        PKMPokemonShape.fetchCount,
+        PKMPokemonSpecies.fetchCount,
+        PKMRegion.fetchCount,
+        PKMStat.fetchCount,
+        PKMSuperContestEffect.fetchCount,
+        PKMType.fetchCount,
+        PKMVersion.fetchCount,
+        PKMVersionGroup.fetchCount,
+    ]
     
     override class func setUp() {
         super.setUp()
@@ -17,22 +69,27 @@ class _PokemonKitTests: XCTestCase {
         PokemonKit._baseURL = "http://127.0.0.1:8000/api/v2"
     }
     
-    func testList() {
-        let expectation = XCTestExpectation(description: "List")
-        
-        PKMPokemon.fetchList { (result) in
-            switch result {
-            case .success(let list):
-                guard let string = list.results.first?.url,
-                    let url = URL(string: string) else { return }
-                print(url.lastPathComponent)
-                expectation.fulfill()
-            case .failure(let error):
-                return
+    
+    /// Decode all count of all resources.
+    /// Implicitly tests decoding the resource list of every type.
+    func testListCount() {
+        let expectations: [XCTestExpectation] = list.enumerated().map({ (offset, element) in
+            let expectation = XCTestExpectation(description: "List \(offset)")
+            
+            element { (result) in
+                switch result {
+                case let .success(count):
+                    print("List \(offset) counts \(count)")
+                    expectation.fulfill()
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
             }
-        }
+            
+            return expectation
+        })
         
-        wait(for: [expectation], timeout: 10.0)
+        wait(for: expectations, timeout: 5.0)
     }
     
     func testParameter() {
