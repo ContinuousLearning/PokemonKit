e1aac109fdb3f543722e60e77a25ee3a633b8808
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-05-22 19:24:47 +0200

    Fix access control

--- Sources/PokemonKit/Types+Resource.swift
+++ Sources/PokemonKit/Types+Resource.swift
@@ -1,236 +1,236 @@
 // PokemonKit 2020
 
 extension PKMAbility: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/ability/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/ability/" }
 }
 
 extension PKMBerry: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/berry/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/berry/" }
 }
 
 extension PKMBerryFirmness: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/berry-firmness/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/berry-firmness/" }
 }
 
 extension PKMBerryFlavour: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/berry-flavor/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/berry-flavor/" }
 }
 
 extension PKMCharacteristic: Resource {
-    typealias List = PKMAPIResourceList
-    static var path: String { "/characteristic/" }
+    public typealias List = PKMAPIResourceList
+    public static var path: String { "/characteristic/" }
 }
 
 extension PKMContestEffect: Resource {
-    typealias List = PKMAPIResourceList
-    static var path: String { "/contest-effect/" }
+    public typealias List = PKMAPIResourceList
+    public static var path: String { "/contest-effect/" }
 }
 
 extension PKMContestType: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/contest-type/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/contest-type/" }
 }
 
 extension PKMEggGroup: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/egg-group/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/egg-group/" }
 }
 
 extension PKMEncounterCondition: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/encounter-condition/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/encounter-condition/" }
 }
 
 extension PKMEncounterConditionValue: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/encounter-condition-value/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/encounter-condition-value/" }
 }
 
 extension PKMEncounterMethod: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/encounter-method/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/encounter-method/" }
 }
 
 extension PKMEvolutionChain: Resource {
-    typealias List = PKMAPIResourceList
-    static var path: String { "/evolution-chain/" }
+    public typealias List = PKMAPIResourceList
+    public static var path: String { "/evolution-chain/" }
 }
 
 extension PKMEvolutionTrigger: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/evolution-trigger/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/evolution-trigger/" }
 }
 
 extension PKMGender: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/gender/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/gender/" }
 }
 
 extension PKMGeneration: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/generation/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/generation/" }
 }
 
 extension PKMGrowthRate: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/growth-rate/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/growth-rate/" }
 }
 
 extension PKMItem: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/item/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/item/" }
 }
 
 extension PKMItemAttribute: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/item-attribute/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/item-attribute/" }
 }
 
 extension PKMItemCategory: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/item-category/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/item-category/" }
 }
 
 extension PKMItemFlingEffect: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/item-fling-effect/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/item-fling-effect/" }
 }
 
 extension PKMItemPocket: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/item-pocket/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/item-pocket/" }
 }
 
 extension PKMLanguage: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/language/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/language/" }
 }
 
 extension PKMLocation: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/location/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/location/" }
 }
 
 extension PKMLocationArea: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/location-area/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/location-area/" }
 }
 
 extension PKMMove: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move/" }
 }
 
 extension PKMMoveAilment: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move-ailment/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move-ailment/" }
 }
 
 extension PKMMoveBattleStyle: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move-battle-style/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move-battle-style/" }
 }
 
 extension PKMMoveCategory: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move-category/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move-category/" }
 }
 
 extension PKMMoveDamageClass: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move-damage-class/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move-damage-class/" }
 }
 
 extension PKMMoveLearnMethod: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move-learn-method/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move-learn-method/" }
 }
 
 extension PKMMoveTarget: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/move-target/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/move-target/" }
 }
 
 extension PKMNature: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/nature/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/nature/" }
 }
 
 extension PKMPalParkArea: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pal-park-area/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pal-park-area/" }
 }
 
 extension PKMPokeathlonStat: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokeathlon-stat/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokeathlon-stat/" }
 }
 
 extension PKMPokedex: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokedex/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokedex/" }
 }
 
 extension PKMPokemon: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokemon/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokemon/" }
 }
 
 extension PKMPokemonColor: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokemon-color/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokemon-color/" }
 }
 
 extension PKMPokemonForm: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokemon-form/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokemon-form/" }
 }
 
 extension PKMPokemonHabitat: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokemon-habitat/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokemon-habitat/" }
 }
 
 extension PKMPokemonShape: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokemon-shape/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokemon-shape/" }
 }
 
 extension PKMPokemonSpecies: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/pokemon-species/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/pokemon-species/" }
 }
 
 extension PKMRegion: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/region/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/region/" }
 }
 
 extension PKMStat: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/stat/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/stat/" }
 }
 
 extension PKMSuperContestEffect: Resource {
-    typealias List = PKMAPIResourceList
-    static var path: String { "/super-contest-effect/" }
+    public typealias List = PKMAPIResourceList
+    public static var path: String { "/super-contest-effect/" }
 }
 
 extension PKMType: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/type/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/type/" }
 }
 
 extension PKMVersion: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/version/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/version/" }
 }
 
 extension PKMVersionGroup: Resource {
-    typealias List = PKMNamedAPIResourceList
-    static var path: String { "/version-group/" }
+    public typealias List = PKMNamedAPIResourceList
+    public static var path: String { "/version-group/" }
 }
