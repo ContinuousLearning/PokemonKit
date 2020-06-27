a2d2b5d90828070f2c04de78c588187228417d90
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-06-27 11:57:14 +0200

    Refactor Resource

--- Sources/PokemonKit/Resource.swift
+++ Sources/PokemonKit/Resource.swift
@@ -12,94 +12,20 @@ public protocol Resource {
     static var path: String { get }
 }
 
-typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
-
 public extension Resource where Self: Decodable {
     static func fetch(id: String, completion: @escaping (Result<Self, Error>) -> Void) {
-
-        
-        let completionHandler: CompletionHandler = { (data, _, error) in
-            switch (data, error) {
-            case (_, let error?):
-                completion(.failure(error))
-            case (let data?, _):
-                let decoder = JSONDecoder()
-                do {
-                    let decoded = try decoder.decode(Self.self, from: data)
-                    completion(.success(decoded))
-                } catch {
-                    completion(.failure(error))
-                }
-            case (.none, .none):
-                fatalError("Impossible")
-            }
-        }
-        
-        
         let url = URL(string: "\(_baseURL)\(Self.path)\(id)/")!
-        
-        let task = URLSession.shared.dataTask(with: url,
-                                              completionHandler: completionHandler)
-        task.resume()
+        URLSession.shared.jsonTask(with: url, completion: completion)
+    }
+    
+    static func fetch(id: String) -> (@escaping (Result<Self, Error>) -> Void) -> Void {
+        return { handler in Self.fetch(id: id, completion: handler) }
     }
 }
 
 extension Resource where List: Decodable {
     static func fetchList(completion: @escaping (Result<List, Error>) -> Void) {
-
-        
-        let completionHandler: CompletionHandler = { (data, _, error) in
-            switch (data, error) {
-            case (_, let error?):
-                completion(.failure(error))
-            case (let data?, _):
-                let decoder = JSONDecoder()
-                do {
-                    let decoded = try decoder.decode(List.self, from: data)
-                    completion(.success(decoded))
-                } catch {
-                    completion(.failure(error))
-                }
-            case (.none, .none):
-                fatalError("Impossible")
-            }
-        }
-        
-        
         let url = URL(string: "\(_baseURL)\(Self.path)")!
-        
-        let task = URLSession.shared.dataTask(with: url,
-                                              completionHandler: completionHandler)
-        task.resume()
-    }
-}
-
-extension Resource where List: Decodable & HasCount {
-    static func fetchCount(completion: @escaping (Result<Int, Error>) -> Void) {
-
-        
-        let completionHandler: CompletionHandler = { (data, _, error) in
-            switch (data, error) {
-            case (_, let error?):
-                completion(.failure(error))
-            case (let data?, _):
-                let decoder = JSONDecoder()
-                do {
-                    let decoded = try decoder.decode(List.self, from: data)
-                    completion(.success(decoded.count))
-                } catch {
-                    completion(.failure(error))
-                }
-            case (.none, .none):
-                fatalError("Impossible")
-            }
-        }
-        
-        
-        let url = URL(string: "\(_baseURL)\(Self.path)")!
-        
-        let task = URLSession.shared.dataTask(with: url,
-                                              completionHandler: completionHandler)
-        task.resume()
+        URLSession.shared.jsonTask(with: url, completion: completion)
     }
 }
